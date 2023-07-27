fbaea3640d0abbb7e138c7a03f3579ed19fc058b
laoou <jx523644@163.com>
2023-07-27 10:41:19 +0800

    员工管理功能

--- src/main/java/com/laoou/reggie/controller/EmployeeController.java
+++ src/main/java/com/laoou/reggie/controller/EmployeeController.java
@@ -1,18 +1,18 @@
 package com.laoou.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.laoou.reggie.common.R;
 import com.laoou.reggie.entity.Employee;
 import com.laoou.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -56,12 +56,90 @@ public class EmployeeController {
 
     /**
      * 员工退出登录
+     *
      * @param request
      * @return
      */
     @PostMapping("/logout")
-    public R<String>logout(HttpServletRequest request){
+    public R<String> logout(HttpServletRequest request) {
         request.getSession().removeAttribute("employee");
-        return  R.success("退出成功");
+        return R.success("退出成功");
+    }
+
+    /**
+     * 新增员工
+     *
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("新增员工信息:{}", employee.toString());
+//        初始密码MD5加密
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+//        获取用户id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+        employeeService.save(employee);
+        return R.success("新增员工成功");
+    }
+
+    /**
+     * 分页
+     *
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        log.info("page={},pageSize={},name={}", page, pageSize, name);
+//        分页构造器
+        Page pageInfo = new Page(page, pageSize);
+//        条件构造器
+        LambdaQueryWrapper<Employee> qw = new LambdaQueryWrapper();
+//        添加过滤条件
+        qw.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+//        排序条件
+        qw.orderByDesc(Employee::getUpdateTime);
+//        执行
+        employeeService.page(pageInfo, qw);
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 修改员工信息
+     *
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("更新数据:{}", employee.toString());
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empId);
+        employeeService.updateById(employee);
+        return R.success("员工信息修改成功");
+    }
+
+    /**
+     * 根据ID查询员工
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable Long id) {
+        Employee employee = employeeService.getById(id);
+        if (employee != null) {
+            return R.success(employee);
+        }
+        return R.error("没有这个员工信息");
     }
 }
